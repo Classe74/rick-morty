bcbc0f9aad067119e99e3e9e0383ee47bb2dc30d
klelia <clelia.fradella@gmail.com>
2022-11-15 13:05:05 +0100

    updated version

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <template>
     <AppHeader title="Rick and Morty App" />
     <main>
-        <AppSearch :serchtype="searchType" />
-        <CharacterList :characters="characterList" :loading="loading" />
+        <AppSearch @filterchar="getCharacters" />
+        <CharacterList />
+        <div v-if="store.errormessage">
+            <h1> Opps ! Qualcosa è andato storto</h1>
+            <p>{{ store.errormessage }}</p>
+        </div>
+        <ResultCount />
     </main>
-
 </template>
 
 <script>
@@ -12,33 +16,72 @@ import axios from 'axios';
 import AppHeader from './components/AppHeader.vue';
 import AppSearch from './components/AppSearch.vue';
 import CharacterList from './components/CharacterList.vue';
+import ResultCount from './components/ResultCount.vue';
+
+import { store } from './store';
 
 export default {
     components: {
         AppHeader,
         AppSearch,
-        CharacterList
+        CharacterList,
+        ResultCount
     },
     data() {
         return {
-            apiURL: 'https://rickandmortyapi.com/api/character',
-            characterList: [],
-            loading: false,
-            searchType: ''
+            store,
+            endPoint: 'character',
         }
+    },
+    watch: {
+
     },
     methods: {
         getCharacters() {
-            this.loading = true;
-            axios.get(this.apiURL).then(
+            store.errormessage = '';
+            //const apiaurl = (status) ? this.apiURL + '?status=' + status : this.apiURL;
+            let options = null
+            if (store.search.status && store.search.name) {
+                options = {
+                    params: {
+                        status: store.search.status,
+                        name: store.search.name
+                    }
+                }
+            } else if (store.search.status) {
+                options = {
+                    params: {
+                        status: store.search.status
+                    }
+                }
+            } else if (store.search.name) {
+                options = {
+                    params: {
+                        name: store.search.name
+                    }
+                }
+
+            }
+            // if (store.search.status || store.search.name) {
+            //     options = {
+            //         params: {
+            //             ...store.search
+            //         }
+            //     }
+            // }
+            store.loading = true;
+            const apiurl = store.apiURL + this.endPoint;
+            axios.get(apiurl, options).then(
                 (res) => {
-                    this.characterList = [...res.data.results];
-                    console.log(this.characterList)
-                    this.loading = false;
+                    store.characterList = res.data.results;
+                    store.loading = false;
                 }
             ).catch((error) => {
-                this.loading = false;
-                console.log(error);
+                store.characterList.length = 0;
+                store.loading = false;
+                store.errormessage = error.message
+                // console.log(error.message);
+                // console.log(error.response.status);
             })
         }
     },
--- src/components/CharacterList.vue
+++ src/components/CharacterList.vue
@@ -1,10 +1,11 @@
 <template>
     <section class="container">
-        <div v-if="loading">
+        <div v-if="store.loading">
             Sto caricando i dati
         </div>
-        <div class="row" v-if="!loading">
-            <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-5" v-for="(item, index) in characters" :key="item.id">
+        <div class="row" v-if="!store.loading">
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-5" v-for="(item, index) in store.characterList"
+                :key="item.id">
                 <CardComponent :character="item" />
             </div>
         </div>
@@ -12,12 +13,17 @@
 </template>
 
 <script>
+import { store } from '../store';
 import CardComponent from './CardComponent.vue';
 
 export default {
     name: "CharacterList",
-    props: ["characters", "loading"],
-    components: { CardComponent }
+    components: { CardComponent },
+    data() {
+        return {
+            store
+        }
+    }
 }
 </script>
 
